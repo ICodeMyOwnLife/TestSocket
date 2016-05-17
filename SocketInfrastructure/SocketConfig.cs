f1af6d058ddafe961f15609d839af98366d77042
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-05-17 21:42:48 +0700

    First commit

---
+++ SocketInfrastructure/SocketConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+
+namespace SocketInfrastructure
+{
+    public class SocketConfig
+    {
+        #region Methods
+        public static int GetBacklog()
+            => int.Parse(ConfigurationManager.AppSettings["backlog"]);
+
+        public static int GetBufferSize()
+            => int.Parse(ConfigurationManager.AppSettings["bufferSize"]);
+
+        public static string GetIpAddress()
+            => ConfigurationManager.AppSettings["ipAddress"];
+
+        public static int GetPort()
+            => int.Parse(ConfigurationManager.AppSettings["port"]);
+        #endregion
+    }
+}
---
+++ SocketServer/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using SocketInfrastructure;
+
+
+namespace SocketServer
+{
+    public class Server
+    {
+        #region Fields
+        private static int _backlog = SocketConfig.GetBacklog();
+        private static readonly int _bufferSize = SocketConfig.GetBufferSize();
+        private static ManualResetEvent _doneResetEvent = new ManualResetEvent(false);
+        private static readonly string _ipAddress = SocketConfig.GetIpAddress();
+        private static readonly int _port = SocketConfig.GetPort();
+        #endregion
+
+
+        #region Methods
+        public static void StartListening()
+        {
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndPoint = new IPEndPoint(IPAddress.Parse(_ipAddress), _port);
+            var buffer = new byte[_bufferSize];
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(_backlog);
+
+                while (true)
+                {
+                    _doneResetEvent.Reset();
+                    listener.BeginAccept(AcceptCallback, listener);
+                    _doneResetEvent.WaitOne();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            _doneResetEvent.Set();
+            var listener = (Socket)ar.AsyncState;
+            var handler = listener.EndAccept(ar);
+
+        }
+        #endregion
+    }
+}
